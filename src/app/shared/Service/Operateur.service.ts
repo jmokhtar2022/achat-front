f1894947da0bb3d559a6455bdcc49adb64fed347
jmokhtar2022 <mohamedmokhtar.jaafar@esprit.tn>
2023-10-25 22:56:51 +0100

    update

--- src/app/shared/Service/Operateur.service.ts
+++ src/app/shared/Service/Operateur.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OperateurService {
-  readonly API_URL = 'http://back:8089/SpringMVC/operateur';
+  readonly API_URL = 'http://192.168.1.14:6868/SpringMVC/operateur';
 
   constructor(private httpClient: HttpClient) { }
 
--- src/app/shared/Service/Secteur-activite.service.ts
+++ src/app/shared/Service/Secteur-activite.service.ts
@@ -5,7 +5,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class SecteurActiviteService {
-  readonly API_URL = 'http://back:8089/SpringMVC/secteurActivite';
+  readonly API_URL = 'http://192.168.1.14:6868/SpringMVC/secteurActivite';
 
   constructor(private httpClient: HttpClient) { }
 
